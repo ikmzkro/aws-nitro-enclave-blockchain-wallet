9ca5027895f43957211130eddb1166a2993f8cd0
ikmz <ikmz0104.official@gmail.com>
2025-03-31 11:29:50 +0900

    Update key policy template with new conditions and principals

--- scripts/generate_key_policy.sh
+++ scripts/generate_key_policy.sh
@@ -11,7 +11,8 @@ asg_name=$(jq -r '.devNitroWalletEth.ASGGroupName' "${output}")
 instance_id=$(./scripts/get_asg_instances.sh "${asg_name}" | head -n 1)
 
 # pcr_0
-# pcr_0 for debug mode: 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+# pcr_0 for debug mode:
+# 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 pcr_0=$(./scripts/get_pcr0.sh "${instance_id}")
 
 # ec2 role
@@ -28,8 +29,9 @@ jq --arg pcr_0 "$pcr_0" \
    --arg ec2_role_arn "$ec2_role_arn" \
    --arg lambda_execution_arn "$lambda_execution_arn" \
    --arg account_id "arn:aws:iam::${account_id}:root" \
-   '.Statement[0].Condition.StringEqualsIgnoreCase."kms:RecipientAttestation:ImageSha384"=$pcr_0 |
-    .Statement[0].Principal.AWS=$ec2_role_arn |
-    .Statement[1].Principal.AWS=$lambda_execution_arn |
-    .Statement[2].Principal.AWS=$account_id' \
-   ./scripts/kms_key_policy_template.json
+   '
+   .Statement[0].Condition.StringEqualsIgnoreCase."kms:RecipientAttestation:ImageSha384" = $pcr_0 |
+   .Statement[0].Principal.AWS = $ec2_role_arn |
+  .Statement[1].Principal.AWS = $lambda_execution_arn |
+   .Statement[2].Principal.AWS = $account_id
+   ' ./scripts/kms_key_policy_template.json

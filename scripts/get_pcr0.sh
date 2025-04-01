2a7f7a2029c2e020752c1fdcb2b68d98b34ee4e9
ikmz <ikmz0104.official@gmail.com>
2025-04-01 12:21:29 +0900

    Retrieve and verify Nitro Enclave's PCR0 value on EC2

--- scripts/get_pcr0.sh
+++ scripts/get_pcr0.sh
@@ -5,6 +5,9 @@
 set +x
 set -e
 
+# 指定した EC2 インスタンス上で Nitro Enclave を構成している場合に、
+# その Enclave の PCR0（Platform Configuration Register 0）値 を取得して、
+# ホストの整合性（＝セキュリティや信頼性の担保）を検証するために使用
 command_id=$(aws ssm send-command --region ${CDK_DEPLOY_REGION} --document-name "AWS-RunShellScript" --instance-ids ${1} --parameters 'commands=["sudo nitro-cli describe-enclaves | jq -r '.[].Measurements.PCR0'"]' | jq -r '.Command.CommandId')
 pcr_0=$(aws ssm list-command-invocations --region ${CDK_DEPLOY_REGION} --instance-id ${1} --command-id ${command_id} --details | jq -r '.CommandInvocations[0].CommandPlugins[0].Output')
 echo ${pcr_0}
